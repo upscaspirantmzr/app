f3d6dacd8a8709fe6c7ba1bf0c1603e945bf8667
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-07-27 16:28:18 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// firebaseConfig.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB47PN51DxVExryra_tYeW2kuGNK9wmyFo",
+  authDomain: "upscbuddymzr.firebaseapp.com",
+  projectId: "upscbuddymzr",
+  storageBucket: "upscbuddymzr.firebasestorage.app",
+  messagingSenderId: "478927698160",
+  appId: "1:478927698160:web:53fb75ad368005bd0b6a35",
+  measurementId: "G-P9P6K4VVEQ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

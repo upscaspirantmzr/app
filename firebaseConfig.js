f6e47253c3d313151c439a89a79e703abba0f115
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-07-27 16:46:07 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 // firebaseConfig.js
 
-const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyB47PN51DxVExryra_tYeW2kuGNK9wmyFo",
   authDomain: "upscbuddymzr.firebaseapp.com",
   projectId: "upscbuddymzr",
@@ -9,6 +9,3 @@ const firebaseConfig = {
   appId: "1:478927698160:web:53fb75ad368005bd0b6a35",
   measurementId: "G-P9P6K4VVEQ"
 };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
